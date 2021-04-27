6250eb23c72797a6cf61a78a2118209fedd95ab4
lqz1992 <lqz199212@foxmail.com>
2021-04-27 23:15:29 +0800

    fsdf

---
+++ src/main/java/com/cmcc/action/UserAction.java
@@ -0,0 +1,34 @@
+package com.cmcc.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
+public class UserAction {
+
+	public String retMsg;
+	HttpServletRequest request = ServletActionContext.getRequest();
+	HttpServletResponse response = ServletActionContext.getResponse();
+
+	public void finish() throws IOException {
+		response.setContentType("text/text;charset=utf-8");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().print(retMsg);
+	}
+
+	public void login() throws IOException {
+		String userid = request.getParameter("userid");
+		String password = request.getParameter("password");
+
+		if ("admin".equalsIgnoreCase(userid) && "admin".equals(password)) {
+			retMsg = "success";
+		} else {
+			retMsg = "fail";
+		}
+		finish();
+	}
+
+}
