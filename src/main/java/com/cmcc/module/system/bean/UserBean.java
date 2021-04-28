bda731ff18d69f4fab4e902caccb68a659041928
lqz1992 <lqz199212@foxmail.com>
2021-04-28 23:53:08 +0800

    11

--- src/main/java/com/cmcc/module/system/bean/UserBean.java
+++ src/main/java/com/cmcc/module/system/bean/UserBean.java
@@ -1,21 +1,17 @@
 package com.cmcc.module.system.bean;
 
 public class UserBean {
-	private String userid;
+	private String user_id;
 	private String password;
-
-	public String getUserid() {
-		return userid;
+	public String getUser_id() {
+		return user_id;
 	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
